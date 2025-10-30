786e4e7ba9a97e0e613dfc89548527295bbbba46
saadminhas835 <sminhas835@gmail.com>
2025-10-30 21:27:55 +0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.get('/ping', (req, res) => {
+  return res.json({
+    message: "pong - the server is running";
+  });
+});
 app.use("/api", aiRoutes);
 
 export default app;

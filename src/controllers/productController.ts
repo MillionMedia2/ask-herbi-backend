48dc977f318559aa5b2ac6519a47e75f5ffb84f2
thehexatown-saad-minhas <muhammad.saad@thehexatown.com>
2025-11-29 18:08:45 +0500

    just visible and publioshed products

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,48 +1,3 @@
-// import { Request, Response } from "express";
-// import { wooApi } from "../services/wooClient";
-
-// export const getProducts = async (req: Request, res: Response) => {
-//   try {
-//     const { data } = await wooApi.get("products", {
-//       per_page: 100,
-//       page: 1,
-//     });
-
-//     const cleanedProducts = data.map((product: any) => ({
-//       id: product.id,
-//       name: product.name,
-//       slug: product.slug,
-//       permalink: product.permalink,
-//       price: product.price,
-//       regular_price: product.regular_price,
-//       sale_price: product.sale_price,
-//       stock_quantity: product.stock_quantity,
-//       stock_status: product.stock_status,
-//       on_sale: product.on_sale,
-//       category:
-//         product.categories && product.categories.length > 0
-//           ? product.categories[0].name
-//           : null,
-//       brand:
-//         product.brands && product.brands.length > 0
-//           ? product.brands[0].name
-//           : null,
-//       images: product.images.map((img: any) => ({
-//         id: img.id,
-//         src: img.src,
-//       })),
-//     }));
-
-//     res.json(cleanedProducts);
-//   } catch (error: any) {
-//     console.error(
-//       "WooCommerce API Error:",
-//       error.response?.data || error.message
-//     );
-//     res.status(500).json({ error: "Failed to fetch products" });
-//   }
-// };
-
 import { Request, Response } from "express";
 import { wooApi } from "../services/wooClient";
 
@@ -56,6 +11,8 @@ export const getProducts = async (req: Request, res: Response) => {
       const { data } = await wooApi.get("products", {
         per_page: perPage,
         page,
+        status: "publish",
+        catalog_visibility: "visible",
       });
 
       if (!data.length) break; // Stop when no more products
